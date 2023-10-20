57dc35fe83e4c1ed6233270e4d14aa4a8742f0b2
Bryan <bpfahert@gmail.com>
2023-10-20 12:06:46 -0500

    Update bootstrap css display for book components

--- bookshelf/src/components/Book.tsx
+++ bookshelf/src/components/Book.tsx
@@ -14,14 +14,14 @@ export default function Book({ title, author, id, subjects } : BookType) {
 
 
     return (
-        <div className='card text-center border-dark mb-3' style={{width: '16rem'}}>
-            <li>{title}</li>
+        <div className='card text-center border-dark mb-3 d-flex flew- justify-content-start align-items-center' style={{width: '16rem', height: "13rem"}}>
+            <li className='overflow-auto'><h5>{title}</h5></li>
             <li>by {author}</li>
-            <li>{isOnBookshelf(booklist, currentBook) ? 
+            <li className='mt-auto'>{isOnBookshelf(booklist, currentBook) ? 
             <button type='button' className='btn btn-secondary' onClick={() => dispatch(removeBook(currentBook))}>Remove from your bookshelf</button>
             : <button type='button' className='btn btn-secondary' onClick={() => dispatch(addBook(currentBook))}>Add to your bookshelf</button>}
             </li>
-            <li><a href={`https://www.gutenberg.org/ebooks/${id}`}>Get free eBook</a></li>
+            <li className=''><a href={`https://www.gutenberg.org/ebooks/${id}`}>Get free eBook</a></li>
         </div>
     )
 }
